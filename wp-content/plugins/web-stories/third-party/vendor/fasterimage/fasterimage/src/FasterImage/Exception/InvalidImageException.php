76070bb0c0fa482697e46c5eb34a64984717cda3
vinay-ai <vinaykolwankar@gmail.com>
2021-04-27 16:23:30 +0530

    Revert "temp"
    This reverts commit b80dbf65540743a904f0ce567743b7cbc49f5d8e.

--- wp-content/plugins/web-stories/third-party/vendor/fasterimage/fasterimage/src/FasterImage/Exception/InvalidImageException.php
+++ wp-content/plugins/web-stories/third-party/vendor/fasterimage/fasterimage/src/FasterImage/Exception/InvalidImageException.php
@@ -1,1 +1,12 @@
- 
+<?php
+
+namespace Google\Web_Stories_Dependencies\FasterImage\Exception;
+
+/**
+ * Class InvalidImageException
+ *
+ * @package FasterImage\Exception
+ */
+class InvalidImageException extends \Exception
+{
+}
